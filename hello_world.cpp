3032ed11e2796f3004835ce21f820a836d246a76
neddsr <neddsr@gmail.com>
2025-03-12 20:41:11 +0300

    updated now not easy file

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,11 @@
-include <iostream>
-include <string>
-using namespace std;
+#include <iostream>
+#include <string>
 
 int main() 
 {
-  string name;
-  сout << "Hello world";
-  cout << "What is your name?";
-  cin >> name;
-  cout << "Hello world from " << name;
+    std::string name;
+    std::cout << "Hello world";
+    std::cout << "What is your name?";
+    std::cin >> name;
+    std::cout << "Hello world from " << name;
 }
